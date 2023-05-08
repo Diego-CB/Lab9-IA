92a522374c448e8d2b66a407e00806ab3269608c
Diego-ACB <60157586+Nes15442@users.noreply.github.com>
2023-05-07 23:46:36 -0600

    Punch Out finished

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 ## Video task 1 - Frozen Lake -
 - https://www.youtube.com/watch?v=TQ6PDotXEyU
 
-El archivo PunchOut.py tiene el ambiente y unicamente se juega hasta que el jugador (personaje blanco) llega a 10 puntos. Pero no intenta ganar primero, para eso hay que agregar el algoritmo de aprendizaje por refuerzo
-
-El archivo test.py es lo mismo, pero es donde se está intentando agregar el algoritmo de aprendizaje por refuerzo para que el agente intente llegar a los 10 puntos primero. Agregue algo pero no funciona. 
+## Video task 2 - Punch Out -
+- 
--- punchOut.py
+++ punchOut.py
@@ -1,28 +1,61 @@
-# ANTES DE CORRER
-# pip install gymnasium[atari]
-# pip install gymnasium[accept-rom-license]
-
 import gymnasium as gym
+import numpy as np
 
+# Env init
 env = gym.make("ALE/Boxing-v5", render_mode='human')
 observation, info = env.reset()
 
-score = 0
-goal = 3
-observation, info = env.reset(seed=42)
-for _ in range(1000):
-    action = env.action_space.sample()
+# Q-Learning Parameters
+learning_rate = 0.1
+discount_rate = 0.99
+exploration_rate = 1
+max_exploration_rate = 1
+min_exploration_rate = 0.01
+exploration_decay_rate = 0.001
 
-    observation, reward, terminated, truncated, info = env.step(action)
+# Q-Table Parameters
+x_states = 160
+y_states = 210
+n_actions = env.action_space.n
+q_table = np.zeros((x_states * y_states, n_actions))
 
-    if reward > 0:
-        print(f'El agente acertó un golpe de {reward} puntos')
-        score += reward
+# Training Parameters
+total_epochs = 100
+max_steps_per_epoch = 2500
 
-    if terminated or truncated:
-        observation, info = env.reset()
+# Q-Learning Algorithm
+for epoch in range(total_epochs):
+    print('Epoch:', epoch)
+    state, _ = env.reset()
+    done = False
+    step = 0
 
-    if score >= goal:
-        print(f'Tu jugador ha acertado {goal} golpes')
-        break
+    while step < max_steps_per_epoch and not done:
+        # Exploration-Exploitation Trade-Off
+        exploration_rate_threshold = np.random.uniform(0, 1)
+
+        if exploration_rate_threshold > exploration_rate:
+            action = np.argmax(q_table[state, :])
+        else:
+            action = env.action_space.sample()
+
+        # Take action
+        new_state, reward, done, _, info = env.step(action)
+
+        # Update Q-Table
+        q_table[state, action] = q_table[state, action] + learning_rate * \
+            (reward + discount_rate *
+             np.max(q_table[new_state, :]) - q_table[state, action])
+
+        # Transition to next state
+        state = new_state
+
+        # Increment step count
+        step += 1
+
+    # Exploration rate decay
+    exploration_rate = min_exploration_rate + \
+        (max_exploration_rate - min_exploration_rate) * \
+        np.exp(-exploration_decay_rate * epoch)
+
 env.close()
